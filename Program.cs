65f95ed07269f90662d301ade55a263be3946ea0
Branko Straub <brankostraub@Brankos-MacBook-Pro.local>
2018-11-21 17:45:09 +0100

    First Version

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+
+namespace gzip
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new Options();
+            options.ConnectionStringSource = args[0];
+            options.ConnectionStringDestination = args[1];
+            string queueName = args[2];
+            string queueNameLog = args[4];
+            string queueCS = args[3];
+            var storageAccount = CloudStorageAccount.Parse(args[0]);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var msg = ".";
+
+            while(msg != "null"){
+            
+                var retrievedMessage = await queue.GetMessageAsync();
+
+                if(retrievedMessage != null){
+                    msg = retrievedMessage.AsString;
+                }else{
+                    msg = "null";
+                    return;
+                }
+            
+                var container = msg.Split('/')[0];
+                var prefix = msg.Substring(msg.IndexOf('/')+1);
+                Console.WriteLine(container+" "+prefix);
+               
+                var storageAccountS = CloudStorageAccount.Parse(options.ConnectionStringSource);
+                var storageAccountD = CloudStorageAccount.Parse(options.ConnectionStringDestination);
+                
+                var blobClientS = storageAccountS.CreateCloudBlobClient();
+                var blobContainerS = blobClientS.GetContainerReference(container);
+
+                var blobClientD = storageAccountD.CreateCloudBlobClient();
+                var blobContainerD = blobClientD.GetContainerReference("gzip-"+container);
+
+                // Do the compression work
+                await new Utility().EnsureGzipFiles(blobContainerS, blobContainerD, prefix, queueCS,queueName, queueNameLog);
+
+                await queue.DeleteMessageAsync(retrievedMessage);
+                //await Task.Delay(500);
+            }
+
+            stopWatch.Stop();
+            var ts = stopWatch.Elapsed;
+
+            var queueLog = queueClient.GetQueueReference(queueNameLog);
+            var message = new CloudQueueMessage("RunTime " + ts);
+            await queueLog.AddMessageAsync(message);
+        }
+    }
+}
+    
---
+++ Utility.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace gzip
+{
+    class Utility
+    {
+        private long bytesBefore = 0;
+        private long bytesAfter = 0;
+
+        public async Task EnsureGzipFiles(CloudBlobContainer containerS, CloudBlobContainer containerD, string prefix, string queueString, string queueName, string queueNameLog)
+        {
+          
+            var blobInfos = containerS.ListBlobs($"{prefix}/", true, BlobListingDetails.Metadata);
+            //Console.WriteLine("Files: " + blobInfos.Count());
+
+            //2018/april/ RECIBE
+            //2018/april/30/eeee.json MAL
+            //2018/april/eeee.json OK
+
+            blobInfos = blobInfos.Where(x=>x.Uri.AbsolutePath.Substring(1, x.Uri.AbsolutePath.LastIndexOf('/')-1) == $"{containerS.Name}/{prefix}");
+            Console.WriteLine("Files: " + blobInfos.Count());
+
+            foreach(var blobInfo in blobInfos){
+                 await Upload(blobInfo, containerD, queueName, queueString, queueNameLog);
+            }
+
+            //Console.WriteLine("Size Before: " + bytesBefore / 1024 / 1024 + "mb");
+            //Console.WriteLine("Size After: " + bytesAfter / 1024 / 1024 + "mb");
+        }
+
+        public async Task Upload(IListBlobItem blobInfo, CloudBlobContainer containerD, string queueName, string queueString, string queueNameLog){
+            
+                var blob = (CloudBlob)blobInfo; 
+                byte[] compressedBytes;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+                    using (var blobStream = await blob.OpenReadAsync())
+                    {
+                        await blobStream.CopyToAsync(gzipStream);
+                        bytesBefore+=blobStream.Length;
+                    }
+
+                    compressedBytes = memoryStream.ToArray();
+                    bytesAfter+=compressedBytes.Length;
+                }
+
+                containerD.CreateIfNotExistsAsync().Wait();
+                var destinationBlob = containerD.GetBlockBlobReference(blob.Name);
+                
+                if(await destinationBlob.ExistsAsync()){
+                    //Console.WriteLine($"file exists {blob.Name}");
+                    return; 
+                }
+
+                // Upload the compressed bytes to the new blob
+                Trace.TraceInformation("Writing blob: " + blob.Name);
+                try{
+                    await destinationBlob.UploadFromByteArrayAsync(compressedBytes, 0, compressedBytes.Length);
+                }catch(Exception e){
+                    var storageAccount = CloudStorageAccount.Parse(queueString);
+                    var queueClient = storageAccount.CreateCloudQueueClient();
+                    var queue = queueClient.GetQueueReference(queueName);
+                    var message = new CloudQueueMessage(blob.Uri.AbsolutePath.Substring(1, blob.Uri.AbsolutePath.LastIndexOf('/')-1));
+                    await queue.AddMessageAsync(message);
+                    var queuelog = queueClient.GetQueueReference(queueNameLog);
+                    var message2 = new CloudQueueMessage(e.Data+e.Message+e.InnerException);
+                    await queue.AddMessageAsync(message2);
+                    return;
+                }
+
+                // Set the blob headers
+                destinationBlob.Properties.ContentType = blob.Properties.ContentType;
+                destinationBlob.Properties.ContentEncoding = "gzip";
+                destinationBlob.SetProperties();
+                
+        }
+
+    }
+}
