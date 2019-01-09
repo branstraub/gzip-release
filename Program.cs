6d0fdd465157df187586cdd29a1ad5508bd179a9
Federico Gerardo Rafael Báez Petrocelli <fgbaezp@gmail.com>
2019-01-09 16:01:42 -0300

    Also re enqueuing message in case of failure

--- Program.cs
+++ Program.cs
@@ -13,59 +13,72 @@ using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace gzip
 {
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var options = new Options();
-            options.BlobConnectionStringSource = args[0];
-            options.BlobConnectionStringDestination = args[1];
-            options.QueueConnectionString = args[2];
-            options.QueueName = args[3];
-          
-            var storageAccountQueue = CloudStorageAccount.Parse(options.QueueConnectionString);
-            var queueClient = storageAccountQueue.CreateCloudQueueClient();
-            var queue = queueClient.GetQueueReference(options.QueueName);
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var options = new Options();
+			options.BlobConnectionStringSource = args[0];
+			options.BlobConnectionStringDestination = args[1];
+			options.QueueConnectionString = args[2];
+			options.QueueName = args[3];
 
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
-            var msg = ".";
-            
-            //msg = "https://gzipo.blob.core.windows.net/gzipi/2018/april/0/ac583c70-df76-4e02-a70f-7a3d06a0985d.json";
+			var storageAccountQueue = CloudStorageAccount.Parse(options.QueueConnectionString);
+			var queueClient = storageAccountQueue.CreateCloudQueueClient();
+			var queue = queueClient.GetQueueReference(options.QueueName);
 
-            while(msg != "null"){
-            
-                var retrievedMessage = await queue.GetMessageAsync();
-                
-                if(retrievedMessage != null){
-                    msg = retrievedMessage.AsString;
-                    await queue.DeleteMessageAsync(retrievedMessage);
-                }else{
-                    msg = "null";
-                    return;
-                }
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+			var msg = ".";
 
-                var container = msg.Split('/')[3];
-                var fileName = string.Join("/",msg.Split('/').Skip(4));
+			//msg = "https://gzipo.blob.core.windows.net/gzipi/2018/april/0/ac583c70-df76-4e02-a70f-7a3d06a0985d.json";
+
+			while (msg != "null")
+			{
+
+				var retrievedMessage = await queue.GetMessageAsync();
+
+				if (retrievedMessage != null)
+				{
+					msg = retrievedMessage.AsString;
+					await queue.DeleteMessageAsync(retrievedMessage);
+				}
+				else
+				{
+					msg = "null";
+					return;
+				}
+
+				var container = msg.Split('/')[3];
+				var fileName = string.Join("/", msg.Split('/').Skip(4));
 				var yearAndMonth = fileName.Substring(0, 6);
-                var storageAccountS = CloudStorageAccount.Parse(options.BlobConnectionStringSource);
-                var storageAccountD = CloudStorageAccount.Parse(options.BlobConnectionStringDestination);
-                
-                var blobClientS = storageAccountS.CreateCloudBlobClient();
-                var blobContainerS = blobClientS.GetContainerReference(container);
+				var storageAccountS = CloudStorageAccount.Parse(options.BlobConnectionStringSource);
+				var storageAccountD = CloudStorageAccount.Parse(options.BlobConnectionStringDestination);
 
-                var blobClientD = storageAccountD.CreateCloudBlobClient();
-                var blobContainerD = blobClientD.GetContainerReference(container+yearAndMonth+"compressed");
+				try
+				{
+					var blobClientS = storageAccountS.CreateCloudBlobClient();
+					var blobContainerS = blobClientS.GetContainerReference(container);
 
-                // Do the compression work
-                await new Utility().EnsureGzipFiles(blobContainerS, blobContainerD, fileName, options.QueueName, options.QueueConnectionString, msg);
+					var blobClientD = storageAccountD.CreateCloudBlobClient();
+					var blobContainerD = blobClientD.GetContainerReference(container + yearAndMonth + "compressed");
 
-            }
+					// Do the compression work
+					await new Utility().EnsureGzipFiles(blobContainerS, blobContainerD, fileName, options.QueueName, options.QueueConnectionString, msg);
+				}
+				catch (Exception)
+				{
+					var storageAccount = CloudStorageAccount.Parse(options.QueueConnectionString);
+					queueClient = storageAccount.CreateCloudQueueClient();
+					queue = queueClient.GetQueueReference(options.QueueName);
+					var message = new CloudQueueMessage(msg);
+					await queue.AddMessageAsync(message);
+					// Todo write this message to a file in the C disk
+				}
+			}
 
-            stopWatch.Stop();
-            var ts = stopWatch.Elapsed;
-
-        }
-    }
+			stopWatch.Stop();
+			var ts = stopWatch.Elapsed;
+		}
+	}
 }
-    
